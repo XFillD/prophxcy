fb6868a9ab34b024f044e962a518bfa7843fc957
Filip Hauba <haubafilip@gmail.com>
2023-12-18 13:37:04 +0100

    basic supabase setup + env file

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,9 +14,10 @@ export default function Home() {
       </Header>
       <div className="mt-2 mb-7 px-6">
         <div className="flex justify-between items-center">
-          <h1>Newest Beats</h1>
+          <h1 className="text-white text-2xl font-semibold">Newest Beats</h1>
 
         </div>
+        <div>List of Beats</div>
       </div>
     </div>
   );
