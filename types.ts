6694fb503d1e6389c93e31384be9f59edfcbbf14
Filip Hauba <haubafilip@gmail.com>
2023-12-23 11:54:13 +0100

    useUser hook

--- types.ts
+++ types.ts
@@ -1,6 +1,7 @@
+import { type } from "os";
 import Stripe from "stripe";
 
-export interface userDetails {
+export interface UserDetails {
   id: string;
   first_name: string;
   last_name: string;
@@ -9,3 +10,48 @@ export interface userDetails {
   billing_address?: Stripe.Address;
   payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
 }
+
+export interface Product {
+  id: string;
+  active?: boolean;
+  description?: string;
+  name?: string;
+  image?: string;
+  metadata?: Stripe.Metadata;
+}
+
+export interface Price {
+  id: string;
+  active?: boolean;
+  description?: string;
+  product_id?: string;
+  unit_amount?: number;
+  curency?: string;
+  type?: Stripe.Price.Type;
+  interval?: Stripe.Price.Recurring.Interval;
+  interval_count?: number;
+  trial_period_days?: number | null;
+  metadata?: Stripe.Metadata;
+  products?: Product;
+}
+
+export interface Subscription {
+  id: string;
+  user_id: string;
+  status?: Stripe.Subscription.Status;
+  metadata?: Stripe.Metadata;
+  price_id?: string;
+  quantity?: string;
+  cancel_at_period_end?: boolean;
+  created?: string;
+  current_period_end: string;
+  current_period_start: string;
+  ended_at?: string;
+  cancel_at?: string;
+  cancelled_at?: string;
+  trial_end?: string;
+  trial_start?: string;
+  prices?: Price;
+}
+
+
